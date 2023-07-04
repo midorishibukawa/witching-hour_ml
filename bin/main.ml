7f059e6c7658c4a131f39b3045d1c9c7c3f41da1
midorishibukawa <midori@shibukawa.io>
2023-07-04 10:11:30 -0300

    minor adjustments

--- bin/main.ml
+++ bin/main.ml
@@ -2,7 +2,7 @@ let handle_htmx req body =
     match Dream.header req "HX-Request" with 
     | None   -> Dream.html 
              @@ Template.page "The Witching Hour"
-             @@ body
+                body
     | Some _ -> Dream.html body
 ;;
 
@@ -15,54 +15,48 @@ let () =
     @@  D.router [
 
         D.get "/" (fun req ->
-            let nav =   T.nav ["signup"; "signin"] in
+            let nav   = T.nav ["signup"; "signin"] in
             handle_htmx req nav );
 
         D.get "/signup" (fun req ->
-            let form = T.form "POST" "/signup"
-                     [ T.input "username" "text"     true 
-                     ; T.input "email"    "email"    true  
-                     ; T.input "password" "password" true 
-                     ; T.input "passconf" "password" true ]
-                     req in 
+            let form  = T.form  "POST"      "/signup"
+                      [ T.input "username"  "text"      true 
+                      ; T.input "email"     "email"     true  
+                      ; T.input "password"  "password"  true 
+                      ; T.input "passconf"  "password"  true ]
+                      req in 
             handle_htmx req form );
 
 
         D.get "/signin" (fun req ->
-            let form =  T.form "POST" "/signin"
-                      [ T.input "username" "text"     true
-                      ; T.input "password" "password" true ]
+            let form  = T.form  "POST"      "/signin"
+                      [ T.input "username"  "text"      true
+                      ; T.input "password"  "password"  true ]
                       req in
             handle_htmx req form );
 
         D.post "/signup" (fun req ->
-            let module D = Dream in
-            let module T = Template in
             let%lwt body = D.body req in
             match%lwt D.form req with 
-            | `Ok   [ "email",    email
-                    ; "passconf", passconf
-                    ; "password", password
-                    ; "username", username ] ->
-                    D.html
-                 @@ T.ul    [ username 
-                            ; email 
-                            ; password 
-                            ; passconf ]
+            | `Ok [ "email",    _email
+                  ; "passconf", passconf
+                  ; "password", password
+                  ; "username", _username ] 
+                 -> if      password = passconf 
+                    then    D.html @@ T.p "SUCCESS" 
+                    else    D.html ~status:`Bad_Request @@ T.p "ERROR"
             | _  -> D.html ~status:`Bad_Request 
                  @@ T.p body );
 
         D.post "/signin" (fun req ->
-            let module D = Dream in
-            let module T = Template in
             let%lwt body = D.body req in
             match%lwt D.form req with 
-            | `Ok   [ "password", password
-                    ; "username", username ] ->
-                    T.ul    [ username 
+            | `Ok [ "password", password
+                  ; "username", username ]
+                 -> D.html
+                 @@ T.ul    [ username 
                             ; password ]
-                |>  D.html
-            | _ ->  T.p body
-                |>  D.html ~status:`Bad_Request );
+            | _  -> D.html ~status:`Bad_Request
+                 @@ T.p body );
     ]
 
