b32df52ba8938c732a352d3c4557ace15d5a860d
midorishibukawa <midori@shibukawa.io>
2023-07-04 12:24:29 -0300

    minor cleanup

--- bin/main.ml
+++ bin/main.ml
@@ -1,22 +1,25 @@
-let handle_htmx req body =
-    match Dream.header req "HX-Request" with 
-    | None   -> Dream.html 
-             @@ Template.page "The Witching Hour"
-                body
-    | Some _ -> Dream.html body
-;;
-
 let () =
     let module D = Dream in
     let module T = Template in
-        D.run
+    let handle_htmx req body =
+        match D.header req "HX-Request" with 
+        | None   -> D.html 
+                 @@ T.page "The Witching Hour"
+                    body
+        | Some _ -> D.html body 
+    in  D.run
     @@  D.logger
     @@  D.memory_sessions
     @@  D.router [
 
+        (*      HOME        *)
+
         D.get "/" (fun req ->
-            let nav   = T.nav ["signup"; "signin"] in
+            let nav   = T.nav [ "signup"; "signin" ] in
             handle_htmx req nav );
+
+
+        (*      SIGNUP      *)
 
         D.get "/signup" (fun req ->
             let form  = T.form  "POST"      "/signup"
@@ -24,16 +27,8 @@ let () =
                       ; T.input "email"     "email"     true  
                       ; T.input "password"  "password"  true 
                       ; T.input "passconf"  "password"  true ]
-                      req in 
-            handle_htmx req form );
-
-
-        D.get "/signin" (fun req ->
-            let form  = T.form  "POST"      "/signin"
-                      [ T.input "username"  "text"      true
-                      ; T.input "password"  "password"  true ]
-                      req in
-            handle_htmx req form );
+                        req 
+            in handle_htmx req form );
 
         D.post "/signup" (fun req ->
             let%lwt body = D.body req in
@@ -47,6 +42,16 @@ let () =
                     else    D.html ~status:`Bad_Request @@ T.p "ERROR"
             | _  -> D.html ~status:`Bad_Request 
                  @@ T.p body );
+
+
+        (*      SIGNIN      *)
+
+        D.get "/signin" (fun req ->
+            let form  = T.form  "POST"      "/signin"
+                      [ T.input "username"  "text"      true
+                      ; T.input "password"  "password"  true ]
+                        req 
+            in handle_htmx req form );
 
         D.post "/signin" (fun req ->
             let%lwt body = D.body req in
