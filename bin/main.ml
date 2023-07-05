e5e0e12af8d41bc20a9e12b0d7c8d7239c540dbf
midorishibukawa <midori@shibukawa.io>
2023-07-05 17:28:27 -0300

    add named params and signup validation

--- bin/main.ml
+++ bin/main.ml
@@ -4,11 +4,9 @@ module T = Caqti_type
 module WH = Template
 
 let () =
-    let handle_htmx req body =
+    let handle_htmx ~req body =
         match D.header req "HX-Request" with 
-        | None   -> D.html 
-                 @@ WH.page "The Witching Hour"
-                    body
+        | None   -> D.html @@ WH.page ~title:"The Witching Hour" ~body
         | Some _ -> D.html body 
     in  D.run 
     @@  D.logger
@@ -18,49 +16,44 @@ let () =
 
         (*      HOME        *)
 
-        D.get "/" (fun req ->
-            let nav   = WH.nav [ "signup"; "signin" ] in
-            handle_htmx req nav );
+        D.get "/" (fun req -> handle_htmx ~req @@ WH.nav [ "signup"; "signin" ]);
 
 
         (*      SIGNUP      *)
 
-        D.get "/signup" (fun req -> handle_htmx req @@ User.signup_form req);
+        D.get "/signup" (fun req -> handle_htmx ~req @@ User.signup_form ~req);
 
         D.post "/signup" (fun req ->
             let handle_lwt res =
                 match%lwt res with 
-                | Some _ -> D.html @@ WH.p "OK"
-                | None   -> D.html ~status:`Bad_Request @@ WH.p "USER/EMAIL/PASSWORD ALREADY EXISTS"
+                | None       -> D.redirect req "/"
+                | Some error -> D.html ~status:`Bad_Request @@ WH.p error
             in
             match%lwt D.form req with
             | `Ok [ "email",    email
-                  ; "passconf", passconf
                   ; "password", password
                   ; "username", username ] 
-                 -> if      password = passconf 
-                    then    let res = User.signup req username email password in
-                            handle_lwt res
-                    else    D.html ~status:`Bad_Request @@ WH.p "PASSWORDS DO NOT MATCH"
+                 -> let res = User.signup ~req ~username ~email ~password in
+                    handle_lwt res
             | _  -> D.html ~status:`Bad_Request 
                  @@ WH.p "INVALID FORM" );
 
 
         (*      SIGNIN      *)
 
-        D.get "/signin" (fun req -> handle_htmx req @@ User.signin_form req);
+        D.get "/signin" (fun req -> handle_htmx ~req @@ User.signin_form ~req);
 
         D.post "/signin" (fun req ->
             let get_body id = 
                 match id with 
-                | Some id -> D.html @@ WH.p id
+                | Some _ -> D.redirect req "/" 
                 | None   -> D.html ~status:`Not_Found @@ WH.p "NOT FOUND"
             in
             let%lwt body = D.body req in
             match%lwt D.form req with 
             | `Ok [ "password", password
                   ; "username", username ]
-                 -> let%lwt id = User.signin req username password in 
+                 -> let%lwt id = User.signin ~req ~username ~password in 
                     get_body id
             | _  -> D.html ~status:`Bad_Request @@ WH.p body );
     ]
--- bin/user.ml
+++ bin/user.ml
@@ -4,28 +4,34 @@ module T = Caqti_type
 module WH = Template
 
 
-let signup_form req =
-    WH.form  "POST"      "/signup"
-  [ WH.input "username"  "text"      true 
-  ; WH.input "email"     "email"     true  
-  ; WH.input "password"  "password"  true 
-  ; WH.input "passconf"  "password"  true ]
-  req
+let signup_form ~req =
+    WH.form  
+    ~meth:"POST"      
+    ~endpoint:"/signup"
+    ~inputs:[ WH.input "username"  "text"      true 
+            ; WH.input "email"     "email"     true  
+            ; WH.input "password"  "password"  true 
+            ; WH.input "passconf"  "password"  true ]
+    ~params:"not passconf"
+    ~req
 
 
-let signin_form req = 
-    WH.form  "POST"      "/signin"
-  [ WH.input "username"  "text"      true
-  ; WH.input "password"  "password"  true ]
-    req 
+let signin_form ~req = 
+    WH.form 
+    ~meth:"POST"      
+    ~endpoint:"/signin"
+    ~inputs:[ WH.input "username"  "text"      true
+            ; WH.input "password"  "password"  true ]
+    ~params:"*"
+    ~req
 
 
 let add_user = 
     let query = 
         let open Caqti_request.Infix in
             (T.(tup3 string string string) ->. T.unit)
-            "INSERT INTO public.users (username, email, password) values ($1, $2, $3);" in
-        fun username email password (module Db : DB) ->
+            "INSERT INTO public.users (username, email, password) values ($1, $2, $3)" in
+        fun ~username ~email ~password (module Db : DB) ->
             let%lwt unit_or_error = Db.exec query (username, email, password) in 
             Caqti_lwt.or_fail unit_or_error
 
@@ -35,15 +41,27 @@ let get_user =
         let open Caqti_request.Infix in
         (T.(tup2 string string) ->? T.string)
         "SELECT id FROM public.users WHERE username = $1 AND password = $2" in
-    fun username password (module Db : DB) ->
+    fun ~username ~password (module Db : DB) ->
         let%lwt id_or_error = Db.find_opt query (username, password) in
         Caqti_lwt.or_fail id_or_error
 
+let user_email_exists =
+    let query =
+        let open Caqti_request.Infix in
+        (T.(tup2 string string) ->! T.(tup2 bool bool))
+        "SELECT bool_or(username = $1) as username, bool_or(email = $2) as email from public.users" in
+    fun ~username ~email (module Db : DB) ->
+        let%lwt tuple_or_error = Db.find query (username, email) in
+        Caqti_lwt.or_fail tuple_or_error
 
-let signup req username email password =
-    let%lwt () = D.sql req (add_user username email password) in
-    Lwt.return @@ Some ()
 
-let signin req username password = 
-    let%lwt id = D.sql req (get_user username password) in
+let signup ~req ~username ~email ~password =
+    match%lwt D.sql req (user_email_exists ~username ~email) with
+    | true,  _ -> Lwt.return @@ Some "USER ALREADY EXISTS"
+    | false, true -> Lwt.return @@ Some "EMAIL ALREADY EXISTS"
+    | false, false -> let%lwt () = D.sql req (add_user ~username ~email ~password) in
+                      Lwt.return @@ None
+
+let signin ~req ~username ~password = 
+    let%lwt id = D.sql req (get_user ~username ~password) in
     Lwt.return id
